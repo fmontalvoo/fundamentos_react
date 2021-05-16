9eb2174e228dbbc6cea7d9dde0f2d2e13f4b542c
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-16 10:46:35 -0500

    Props.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/props.js
@@ -0,0 +1,12 @@
+// Reemplazar este codigo en index.js
+
+// Los props son argumentos que se pueden pasar a un componente.
+// const App = (props) => <h1>¡Hola {props.nombre}!</h1>;
+
+// Tambien se pueden asignar valores por defecto a los parametros de un componente.
+const App = ({nombre, idioma = 'en'}) => <h1>¡{idioma==="es"?"Hola":"Hello"} {nombre}!</h1>;
+
+// Asigna un valor por defecto a la propiedad idioma.
+// App.defaultProps = {idioma: "es"}
+
+ReactDOM.render(<App nombre="Frank" idioma="es" />, document.getElementById("root"))
